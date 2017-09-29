d31ad975dc3ea1ea5c8c59aa1dd7fc43735b9da5
zhangjing02 <zhangjing574839@163.com>
2017-09-29 13:18:55 +0800

    Initial commit

---
+++ app/src/main/java/com/senssun/menu_animator_demo001/MainFragment.java
@@ -0,0 +1,16 @@
+package com.senssun.menu_animator_demo001;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
